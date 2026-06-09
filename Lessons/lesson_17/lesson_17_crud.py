6ab506b9395227681992cb2316560af15eb262d9
Vadim <vadosdos22@gmail.com>
2026-06-09 21:59:20 +0300

    Домашнее задание № 17 на проверку

--- Lessons/lesson_17/lesson_17_crud.py
+++ Lessons/lesson_17/lesson_17_crud.py
@@ -15,10 +15,18 @@ def receive_json_for_create(id_pet, category_id_pet, category_name_pet, pet_name
             }
 
 
-photo_pet_1 = "https://avatars.mds.yandex.net/i?id=754577ce633ab29ea7ccdbd056f1dc28ffaed6c7-16467138-images-thumbs&ref=rim&n=33&w=120&h=180"
+photo_pet_1 = "https://avatars.mds.yandex.net/i?id=9076ba718c33893b2a67c03c4d412caa3cf16f58-5320607-images-thumbs&n=13"
 photo_pet_2 = "https://avatars.mds.yandex.net/i?id=6d1c911b8dc680c20870908e9549c45c89489102-5858835-images-thumbs&n=13"
 
-data = receive_json_for_create(24051989, 7, "Cat", "Tishka", photo_pet_1, 7, "friendly", "pending")
+data = receive_json_for_create(
+    24051989,
+    7,
+    "Cat",
+    "Tishka",
+    photo_pet_1,
+    7,
+    "friendly",
+    "pending")
 
 
 def create_pet():
@@ -32,7 +40,14 @@ def get_pet(id_pet):
 
 
 def put_pet(id_pet):
-    update_pet_data = receive_json_for_create(id_pet, 6, "Cat", "Cat", photo_pet_2, 6, "agressive", "available")
+    update_pet_data = receive_json_for_create(
+        id_pet,
+        6,
+        "Cat",
+        "Cat",
+        photo_pet_2, 6,
+        "agressive",
+        "available")
     response_put_pet = requests.put(url=f"{BASE_URL}{ENDPOINT_1}", headers=HEADERS, json=update_pet_data)
     return response_put_pet
 
