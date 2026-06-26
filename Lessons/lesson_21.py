057dc2e765fe9b138578082923a783bbf98b24a7
VadimRVA <vadosdos22@gmail.com>
2026-06-27 00:20:02 +0300

    Домашнее задание 21

---
+++ Lessons/lesson_21.py
@@ -0,0 +1,23 @@
+#CSS
+
+button_left = ".bi-chevron-left"
+placeholder_search = ".oxd-input--active"
+menu_dashboard = ".oxd-main-menu-item.active"
+settings = ".bi-gear-fill"
+button_upgrade = ".oxd-glass-button"
+profile = ".oxd-userdropdown-tab"
+title = "span[title='Engineering']"
+widget = ".orangehrm-dashboard-widget:nth-of-type(2)"
+
+#XPath
+
+button_left2 = "//*[contains(@class, 'bi-chevron-left')]"
+placeholder_search2 = "//input[@placeholder='Search']"
+menu_dashboard2 = "//a[@href='/web/index.php/dashboard/index']"
+settings2 = "//i[contains(@class, 'bi-gear-fill')]"
+button_upgrade2 = "//button[contains(@class, 'orangehrm-upgrade-button')]"
+profile2 = "//span[@class='oxd-userdropdown-tab']"
+title2 = "//span[@title='Engineering']"
+widget2 = "(//div[contains(@class, 'oxd-grid-item--gutters')and contains(@class, 'oxd-grid-item')])[2]"
+
+
